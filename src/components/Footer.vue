a5ba4801addfa42f67a61a9ba8525b22ffaf767b
Gersom Alaja <gersomalaja@gmail.com>
2020-05-19 19:57:50 -0500

    Add components - Button - Footer - Header

---
+++ src/components/Footer.vue
@@ -0,0 +1,14 @@
+<template lang='pug'>
+  .c-footer
+    p Copyright ©2020 Nuxt Inc. All rights reserved.
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="stylus">
+  .c-footer
+    text-align: center
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template lang='pug'>
+  .c-header
+    nav
+      h2 {{title}}
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: 'Navbar Principal'
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+  .c-header
+    text-align: center
+</style>
